f87f0a583c076532647b01dc4bc74a204f82cd96
NII Gakunin Cloud <40594865+nii-gakunin-cloud@users.noreply.github.com>
2023-05-30 16:22:39 +0900

    Set exact release date of v1.8.0, and minor-fixese.

--- sinetstream/src/main/java/jp/ad/sinet/stream/android/config/remote/configclient/util/DialogUtil.java
+++ sinetstream/src/main/java/jp/ad/sinet/stream/android/config/remote/configclient/util/DialogUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 National Institute of Informatics
+ * Copyright (C) 2022 National Institute of Informatics
  *
  *  Licensed to the Apache Software Foundation (ASF) under one
  *  or more contributor license agreements.  See the NOTICE file
